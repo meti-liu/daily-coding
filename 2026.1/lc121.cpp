030a61851039535f90a9b37c675b9b399846e456
Starry <140034460+meti-liu@users.noreply.github.com>
2026-01-24 23:02:20 +0800

    lc

--- 2026.1/lc121.cpp
+++ 2026.1/lc121.cpp
@@ -9,13 +9,12 @@ public:
 
     int maxProfit(vector<int>& prices) 
     {
-        int sell=prices[1];
         int buy=prices[0];
-        int mp=sell-buy;
+        int mp=0;
 
         for(int i=1;i<prices.size();i++)
         {
-            sell=max(sell,prices[i]);
+            int sell=prices[i];
             buy=min(buy,prices[i-1]);
             int profit=sell-buy;
             mp=max(mp,profit);
