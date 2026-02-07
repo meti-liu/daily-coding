6ae88e1c6c45e910060ced025e17caf54e9e9693
Starry <140034460+meti-liu@users.noreply.github.com>
2026-02-07 23:03:32 +0800

    lc73

---
+++ 2026.2/lc73.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution 
+{
+public:
+    void setZeroes(vector<vector<int>>& matrix) 
+    {
+        int rows=matrix.size();
+        int cols=matrix[0].size();
+        bool rowzero=false;
+        bool colzero=false;
+
+        for(int i=0;i<rows;i++)
+        for(int j=0;j<cols;j++)
+        {
+            if(matrix[i][j]==0)
+            {
+                matrix[0][j]=0;
+                matrix[i][0]=0;
+                if(i==0) rowzero=true;
+                if(j==0) colzero=true;
+
+            }
+        }
+
+        for(int j=1;j<cols;j++)
+        {
+            if(matrix[0][j]==0)
+            {
+                for(int i=1;i<rows;i++) matrix[i][j]=0;
+            }
+        }
+
+        for(int i=1;i<rows;i++)
+        {
+            if(matrix[i][0]==0)
+            {
+                for(int j=1;j<cols;j++) matrix[i][j]=0;
+            }
+        }
+
+
+        for(int i=0;colzero&&i<rows;i++) matrix[i][0]=0;
+        for(int j=0;rowzero&&j<cols;j++) matrix[0][j]=0;
+
+
+        return;
+    }
+};
