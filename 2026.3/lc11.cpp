ef8b115208f27e6febee1fed35db932595d007e2
Starry <140034460+meti-liu@users.noreply.github.com>
2026-03-05 18:54:39 +0800

    lc11

--- 2026.3/lc11.cpp
+++ 2026.3/lc11.cpp
@@ -1,5 +1,6 @@
 #include<vector>
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 class Solution 
@@ -7,6 +8,18 @@ class Solution
 public:
     int maxArea(vector<int>& height) 
     {
+        int i=0;
+        int j=height.size()-1;
+        int res=0;
+        while(i<j)
+        {
+            int h=min(height[i],height[j]);
+            int w=j-i;
+            res=max(res,w*h);
+            if(height[i]>height[j]) j--;
+            else i++;
+        }
+        return res;
         
     }
 };
